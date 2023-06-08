c22715b87feac9e5e869fe2e4463c36468dea4a2
Ashok.Y <113377234+ashok0706@users.noreply.github.com>
2023-06-08 19:05:58 +0530

    Update weather.java

--- WeatherForecast/src/main/java/com/test/WeatherForecast/Model/weather.java
+++ WeatherForecast/src/main/java/com/test/WeatherForecast/Model/weather.java
@@ -4,11 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class weather {
 
 	
