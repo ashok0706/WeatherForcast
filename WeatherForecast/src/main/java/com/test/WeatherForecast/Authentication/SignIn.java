db18a66cbadbe1718f2432cc3e94f0dfb071a42d
Ashok.Y <113377234+ashok0706@users.noreply.github.com>
2023-06-08 19:04:10 +0530

    Update SignIn.java

--- WeatherForecast/src/main/java/com/test/WeatherForecast/Authentication/SignIn.java
+++ WeatherForecast/src/main/java/com/test/WeatherForecast/Authentication/SignIn.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.OneToMany;
-
+import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import com.test.WeatherForecast.Model.weather;
 
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignIn {
 
 	private String userName;
